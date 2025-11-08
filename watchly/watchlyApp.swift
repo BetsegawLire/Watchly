7df72a9e45f211b01b947f2b48411ec4a3a93824
BetsegawLire <betsegawhazard@gmail.com>
2025-11-08 12:04:12 +0300

    Initial Commit

---
+++ watchly/watchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchlyApp.swift
+//  watchly
+//
+//  Created by MacBook Pro on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct watchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
