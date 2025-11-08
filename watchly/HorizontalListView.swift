258c30f1ce12f0e13de106e734eaeb8aeab7e527
BetsegawLire <betsegawhazard@gmail.com>
2025-11-08 13:45:19 +0300

    create ahorizontal list view

---
+++ watchly/HorizontalListView.swift
@@ -0,0 +1,41 @@
+//
+//  HorizontalListView.swift
+//  watchly
+//
+//  Created by MacBook Pro on 08/11/2025.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    let header = Constants.trendingMovieString
+    var titles = [Constants.testTitleURL,Constants.testTitleURL,Constants.testTitleURL]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(header)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(titles, id: \.self) { title in
+                        AsyncImage(url: URL(string: title)) { image in
+                                image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 120,height: 200)
+                    }
+                }
+            }
+        }
+        .frame(height: 250)
+        .padding(10)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
